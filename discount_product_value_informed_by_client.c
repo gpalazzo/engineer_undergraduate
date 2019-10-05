cc29169895f513052f9f911664080c8eb8128eef
gui_palazzo <palazzoeng1@gmail.com>
2019-10-05 12:11:44 -0300

    undergrad C exercise

---
+++ discount_product_value_informed_by_client.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int code;
+float value, perc_discount, price;
+
+main ()
+{
+	printf ("Inform a code to the product:\n");
+	scanf ("%d", &code);
+	printf ("Product value:\n");
+	scanf ("%f", &value);
+	printf ("Discount in percentage:\n");
+	scanf ("%f", &perc_discount);
+	
+	if ( perc_discount <= 30 ) {
+		price = ((100-perc_discount)/100)*value;
+	}
+
+	else {
+		printf ("Maximum discount is 30 per cent, so we will use it.\n");
+		price = (value*0.7);
+	}
+
+	printf ("Product value without discount: %.2f\n",  value);
+	printf ("Product value with discount: %.2f\n", price);
+	
+	system ("pause");	
+}
